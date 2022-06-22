887a69c2c315dd2e9f085154f76569e3163a8fdb
DarKNeSsJuaN25 <68095284+DarKNeSsJuaN25@users.noreply.github.com>
2022-06-21 22:25:31 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from html import entities
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import sys
 from sqlalchemy import sql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/software2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Softjuandius_25@localhost:5432/software2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -21,11 +22,16 @@ db.create_all()
 def authenticate_user():
     error = False
     response = {}
+    print(request.get_json())
     try:
+        print("hola")
         username = request.get_json()['username']
+        print(username)
         password = request.get_json()['password']
+        print(password)
         db.session.query(Usuario).filter(Usuario.nombre==username).filter(Usuario.contraseña==password).one()
     except:
+        print("adios")
         error = True
         db.session.rollback()
         print(sys.exc_info())
@@ -34,9 +40,18 @@ def authenticate_user():
     if error:
         response['error_message'] = "Usuario o contraseña incorrecto"
     response['error'] = error
+        
     return jsonify(response)
 
-
+'''
+@app.route('/test',methods=['GET'])
+def probar_test():
+    db_session = db.getSession()
+    user = entities.Usuario(
+        username = "dion",
+        password = "0"
+    )
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
