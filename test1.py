3e1c561fcdd69c6d72c3b35279eab3da7b11f6a0
DarKNeSsJuaN25 <68095284+DarKNeSsJuaN25@users.noreply.github.com>
2022-06-22 18:21:33 -0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+# Crear una sesión de Firefox
+
+driver = webdriver.Chrome() 
+options = webdriver.ChromeOptions()
+
+# Acceder a la aplicación web
+driver.get('http://127.0.0.1:5003')
+
+def validar_login():
+    #assert "Login"in driver.title
+
+    username = driver.find_element(By.ID,"username")
+    print(username)
+    username.clear()
+    username.send_keys("dion")
+
+    password = driver.find_element(By.ID, "password")
+    password.clear()
+    password.send_keys("0")
+
+    driver.find_element(By.ID, "submit").click()
+
+    assert username=="dion" and password=="0" in driver.page_source, "Usuario logeado con exito!"
+
+
+def login_invalido():
+
+    username = driver.find_element(By.ID,"username")
+    username.clear()
+    username.send_keys("Juan Diego")
+
+    password = driver.find_element(By.ID, "password")
+    password.clear()
+    password.send_keys("0")
+    driver.find_element(By.ID, "submit").click()
+    
+    assert username!="dion" and password != "0" in driver.page_source, "Contrasenia y Usuario incorrectos"
+
+
+def usuario_incorrecto():
+    username = driver.find_element(By.ID, 'username')
+    username.clear()
+    username.send_keys('diona')
+    password = driver.find_element(By.ID,"password")
+    password.clear()
+    password.send_keys("0")
+    driver.find_element(By.ID, "submit").click()
+
+    assert username != "dion" and password ==  "0" in driver.page_source, "Usuario incorrecto"
+
+def contrasenia_incorrecta():
+    username = driver.find_element(By.ID, 'username')
+    username.clear()
+    username.send_keys('dion')
+    password = driver.find_element(By.ID,"password")
+    password.clear()
+    password.send_keys("01")
+    driver.find_element(By.ID, "submit").click()
+
+    assert username == "dion" and password !=  "0" in driver.page_source, "Contrasenia incorrecta"
+
+
+#validar_login()
+#login_invalido()
+#contrasenia_incorrecta()
+#usuario_incorrecto()
